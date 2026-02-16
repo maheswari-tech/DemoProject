fd68ebb21059292ddcfe3ab5fd42d52efa33c5c7
Santhyasenthil <2k22cse133@kiot.ac.in>
2026-02-16 14:49:43 +0530

    Added English Amazon Hero Section component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Hero from './components/Hero/Hero';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Navbar removed as requested */}
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
